b959cc74895777c57d2ebe4e8bde484091af28c7
miketabb33 <miketabb33@gmail.com>
2024-07-27 13:12:48 -0400

    Add back to top button

--- src/react/pages/CatalogPage.tsx
+++ src/react/pages/CatalogPage.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { PageLayout } from '../components/Layout'
 import Catalog from '../components/catalog/Catalog'
 import Navigation from '../components/navigation/Navigation'
+import ScrollToTopButton from '../components/ScrollToTopButton'
 
 const CatalogPage = () => {
   return (
     <PageLayout>
       <Navigation />
       <Catalog />
+      <ScrollToTopButton />
     </PageLayout>
   )
 }
--- src/react/pages/CollectionPage.tsx
+++ src/react/pages/CollectionPage.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { PageLayout } from '../components/Layout'
 import Navigation from '../components/navigation/Navigation'
 import Collection from '../components/collection/Collection'
+import ScrollToTopButton from '../components/ScrollToTopButton'
 
 const CollectionPage = () => {
   return (
     <PageLayout>
       <Navigation />
       <Collection />
+      <ScrollToTopButton />
     </PageLayout>
   )
 }
