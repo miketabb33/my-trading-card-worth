bb5a5e00c13384c30398d431f9e6cd3cb6578d8d
Michael Tabb <miketabb33@gmail.com>
2026-02-16 17:36:08 -0500

    remove expansions

--- src/server/database/repository/ExpansionOrderCRUD.ts
+++ src/server/database/repository/ExpansionOrderCRUD.ts
@@ -1,25 +1,5 @@
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { Schema, model } from 'mongoose'
+import { prisma } from '../prismaClient'
 import { ExpansionOrder } from '../../types/ExpansionOrder'
-
-const expansionOrderSchema = new Schema(
-  {
-    mainSeries: {
-      type: Array,
-      required: true,
-    },
-    otherSeries: {
-      type: Array,
-      required: true,
-    },
-  },
-  { timestamps: true }
-)
-
-const ExpansionOrderModel = model('expansion_order', expansionOrderSchema)
 
 export interface IExpansionOrderCRUD {
   get: () => Promise<ExpansionOrder | null>
@@ -27,15 +7,13 @@ export interface IExpansionOrderCRUD {
 
 class ExpansionOrderCRUD implements IExpansionOrderCRUD {
   get = async (): Promise<ExpansionOrder | null> => {
-    const context = await ExpansionOrderModel.find()
-    if (!context[0]) return null
+    const record = await prisma.expansionPokemonOrder.findFirst()
+    if (!record) return null
 
-    const expansionOrder: ExpansionOrder = {
-      mainSeries: context[0].mainSeries,
-      otherSeries: context[0].otherSeries,
+    return {
+      mainSeries: record.mainSeries as string[],
+      otherSeries: record.otherSeries as string[],
     }
-
-    return expansionOrder
   }
 }
 
