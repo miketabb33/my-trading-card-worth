79cab7ad36079511479313cfbbd38939cceab80a
Michael Tabb <miketabb33@gmail.com>
2026-02-23 08:22:45 -0500

    fix constructor mocks

--- test/server/clients/CardTrader/__MOCKS__/CardTraderBlueprintDto.mock.ts
+++ test/server/clients/CardTrader/__MOCKS__/CardTraderBlueprintDto.mock.ts
@@ -1,42 +1,13 @@
 import { CardTraderBlueprintDto } from '../../../../../src/server/clients/CardTrader/parseBlueprints'
 
-type MakeBlueprintDto = {
-  id?: number
-  name?: string
-  version?: string
-  collectorNumber?: string
-  pokemonRarity?: string
-  imageUrlPreview?: string
-  imageUrlShow?: string
-  categoryId?: number
-}
-
-export const makeBlueprintDto = ({
-  id = 1,
-  name = 'any',
-  version = '',
-  collectorNumber = '',
-  pokemonRarity = 'Common',
-  imageUrlPreview = '',
-  imageUrlShow = '',
-  categoryId = 0,
-}: MakeBlueprintDto): CardTraderBlueprintDto => ({
-  id,
-  name,
-  version,
+export const makeBlueprintDto = (overrides: Partial<CardTraderBlueprintDto> = {}): CardTraderBlueprintDto => ({
+  id: 1,
+  name: 'any',
+  version: '',
   gameId: 0,
-  categoryId,
+  categoryId: 0,
   expansionId: 0,
-  fixedProperties: {
-    collectorNumber,
-    pokemonRarity,
-  },
-  image: {
-    show: {
-      url: imageUrlShow,
-    },
-    preview: {
-      url: imageUrlPreview,
-    },
-  },
+  fixedProperties: { collectorNumber: '', pokemonRarity: 'Common' },
+  image: { show: { url: '' }, preview: { url: '' } },
+  ...overrides,
 })
--- test/server/clients/CardTrader/__MOCKS__/CardTraderMarketplaceProductDto.mock.ts
+++ test/server/clients/CardTrader/__MOCKS__/CardTraderMarketplaceProductDto.mock.ts
@@ -1,25 +1,10 @@
 import { CardTraderMarketplaceProductDto } from '../../../../../src/server/clients/CardTrader/parseMarketplaceProducts'
 
-type MakeMarketplaceProductDtoArgs = {
-  blueprintId?: number
-  priceCents?: number
-  condition?: string | null
-}
-
-export const makeMarketplaceProductDto = ({
-  blueprintId = 0,
-  priceCents = 0,
-  condition = null,
-}: MakeMarketplaceProductDtoArgs): CardTraderMarketplaceProductDto => {
-  const product: CardTraderMarketplaceProductDto = {
-    blueprintId,
-    price: {
-      cents: priceCents,
-    },
-    propertiesHash: {
-      condition,
-    },
-  }
-
-  return product
-}
+export const makeMarketplaceProductDto = (
+  overrides: Partial<CardTraderMarketplaceProductDto> = {}
+): CardTraderMarketplaceProductDto => ({
+  blueprintId: 0,
+  price: { cents: 0 },
+  propertiesHash: { condition: null },
+  ...overrides,
+})
